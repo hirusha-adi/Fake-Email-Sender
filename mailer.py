cd8be04f8c5d3e745fc57494cf8773e00a3f8203
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-02-24 23:12:34 +0530

    send request + improved errors(prolly buggy)

--- mailer.py
+++ mailer.py
@@ -5,6 +5,45 @@ import sys
 import requests
 
 
+# Custom Exceptions
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
+
+
+class MissingSendTo(Error):
+    """Missing email address to send to"""
+
+    def __init__(self):
+        if (__name__ == "__main__"):
+            sys.exit("Missing email address to send to")
+
+    def __str__(self) -> str:
+        return "Missing email address to send to"
+
+
+class MissingSubject(Error):
+    """Missing email subject"""
+
+    def __init__(self):
+        if (__name__ == "__main__"):
+            sys.exit("Missing email subject")
+
+    def __str__(self) -> str:
+        return "Missing email subject"
+
+
+class MissingContent(Error):
+    """Missing Email Content"""
+
+    def __init__(self):
+        if (__name__ == "__main__"):
+            sys.exit("Missing Email Content")
+
+    def __str__(self) -> str:
+        return "Missing Email Content"
+
+
 class AnonymousEmail:
     def __init__(self, e_send_to: str = None,
                  e_subject: str = None,
@@ -12,6 +51,20 @@ class AnonymousEmail:
         self.e_send_to = e_send_to
         self.e_subject = e_subject
         self.e_content = e_content
+
+        self.api_link = "http://anonymouse.org/cgi-bin/anon-email.cgi"
+        self.user_agent = 'Mozilla/5.0 (Linux; Android 6.0.1; HTC6545LVW Build/MMB29M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/85.0.4183.101 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/283.0.0.16.120;]'
+        self.headers = {
+            'Host': 'anonymouse.org',
+            'User-Agent': self.user_agent,
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate',
+            'Referer': 'http://anonymouse.org/anonemail.html',
+            'Connection': 'close',
+            'Upgrade-Insecure-Requests': '1',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
 
     def setSendTo(self, e_send_to: str = None):
         if e_send_to is None:
@@ -27,50 +80,45 @@ class AnonymousEmail:
 
     def setContent(self, e_content: str = None):
         if e_content is None:
-            self.e_content = input("[Content]: ")
+            print("[Content] (enter 'done' when done)")
+            final_content = ""
+            count = 0
+            while True:
+                count += 1
+                temp = input(f"{count}: ")
+                if temp.strip().lower().startswith("done"):
+                    break
+            self.e_content = final_content
         else:
             self.e_content = e_content
 
     def getData(self):
         if self.e_send_to is None:
-            pass
+            raise MissingSendTo
 
         if self.e_subject is None:
-            pass
+            raise MissingSubject
 
         if self.e_content is None:
-            pass
+            raise MissingContent
 
-        data = {
+        return {
             'to': self.e_send_to,
             'subject': self.e_subject,
             'text': self.e_content
         }
 
+    def sendEmail(self, display_info: bool = True):
+        session = requests.Session()
+        rth = session.post(self.api_link,
+                           headers=self.headers,
+                           data=self.getData())
 
-api_link = "http://anonymouse.org/cgi-bin/anon-email.cgi"
-user_agent = 'Mozilla/5.0 (Linux; Android 6.0.1; HTC6545LVW Build/MMB29M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/85.0.4183.101 Mobile Safari/537.36 [FB_IAB/Orca-Android;FBAV/283.0.0.16.120;]'
-headers = {
-    'Host': 'anonymouse.org',
-    'User-Agent': user_agent,
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Accept-Encoding': 'gzip, deflate',
-    'Referer': 'http://anonymouse.org/anonemail.html',
-    'Connection': 'close',
-    'Upgrade-Insecure-Requests': '1',
-    'Content-Type': 'application/x-www-form-urlencoded'
-}
-
-
-session = requests.Session()
-rth = session.post(api_link,
-                   headers=headers,
-                   data=data)
-
-if '200' in rth.text:
-    print("[+] Sending Email")
-else:
-    print("[+] Sending Email")
-    print("[+] In order to increase your privacy, the anonymous e-mail will be randomly delated upto 12 hours")
-    sys.exit()
+        if display_info:
+            if '200' in rth.text:
+                print("[+] Sending Email")
+            else:
+                print("[+] Sending Email")
+                print(
+                    "[+] In order to increase your privacy, the anonymous e-mail will be randomly delated upto 12 hours")
+                sys.exit()
